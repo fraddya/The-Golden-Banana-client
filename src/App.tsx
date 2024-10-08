188e1bee024354723ad428a62d0e70d14442979f
fraddy <ssisithasiranga@gmail.com>
2024-10-08 15:30:15 +0530

    clean un nessury files from project

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
 import Dashboard from './pages/Dashboard';
-import Job from './pages/Job';
-import Employee from './pages/Employee';
-import Brand from './pages/Brand';
-import VehiclePart from './pages/VehiclePart';
-import Home from './pages/Home';
 
 const App: React.FC = () => {
   
@@ -15,11 +10,6 @@ const App: React.FC = () => {
       <Routes>
         <Route path="/" element={<LoginPage />} />
         <Route path="/dashboard" element={<Dashboard />}>
-          <Route path="home" element={<Home />} />
-          <Route path="job" element={<Job />} />
-          <Route path="employee" element={<Employee />} />
-          <Route path="brand" element={<Brand />} />
-          <Route path="vehicle-part" element={<VehiclePart />} />
         </Route>
       </Routes>
     </Router>
